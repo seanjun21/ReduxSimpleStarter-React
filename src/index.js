9072083ed391985c17eae73bfdeb337a47ae1261
Sean Jun <seanjun21@gmail.com>
2018-08-31 01:16:41 -0700

    1 An Intro to React section complete

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import API_KEY from '../config/keys';
+import SearchBar from './components/search_bar';
+
 // Create a new component. This component should produce some HTML
 const App = () => {
-  return <div>Hi!</div>;
-}
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
+};
 
 // Take this component's generated HTML and put it on the page (in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
